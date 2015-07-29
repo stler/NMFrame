f954df5325ce03a485cd2398dbdde93be416509e
stler <moscowdarkside@gamil.com>
2015-07-30 01:55:04 +0400

    Vendor Update

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
+    'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Model\\' => array($baseDir . '/user/Model'),
     'Application\\' => array($baseDir . '/app/Application'),
 );
